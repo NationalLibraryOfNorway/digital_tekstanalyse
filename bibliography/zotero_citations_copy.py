a015935bd855cda3595af5b3159d18ada28c50dd
Ingerid Dale <ingerid.dale@nb.no>
2025-01-28 15:36:28 +0100

    style: format python script with ruff

--- bibliography/zotero_citations_copy.py
+++ bibliography/zotero_citations_copy.py
@@ -13,84 +13,87 @@ def format_newspaper_article_citation(item: dict) -> str:
 
     Surname, Initial(s) (Year) Article Title, *Newspaper Title in italic*. Tilgjengelig fra: URL (Accessed: date).
     """
-    authors = format_creators(item.get("creators"))
-    year = format_date(item.get("date"))
-    url = format_url(item)
-    return f"{authors} ({year}) {item.get('title')}, *{item.get('publicationTitle')}*. {url}"
+    elements = [
+        format_creators(item.get("creators")),
+        f"({format_date(item.get('date'))})",
+        f'"{item.get("title")}",',
+        f"*{item.get('publicationTitle')}*.",
+        format_url(item),
+    ]
+    return elements
 
 
 def format_thesis_citation(item) -> str:
     """Surname, Initial(s). (Year) Thesis Title in italic. Type of thesis. Institution."""
-    citation_elements = []    
-    citation_elements.append(format_creators(item.get("creators")))
+    elements = [
+        format_creators(item.get("creators")),
+        f"({format_date(item.get('date'))})",
+        f"*{item.get('title')}*.",
+    ]
 
-    if (year := format_date(item.get("date"))): 
-        citation_elements.append(f"({year})")
-        
-    if title := item.get("title"):
-        citation_elements.append(f"*{title}*.")
-        
     if thesis_type := item.get("thesisType"):
-        citation_elements.append(thesis_type + ".")
-    
-    if institution := item.get("university"):
-        citation_elements.append(institution + ".")
-    citation_elements.append(format_url(item))
+        elements.append(thesis_type + ".")
 
-    return " ".join(elem for elem in citation_elements if elem != "")
+    if institution := item.get("university"):
+        elements.append(institution + ".")
+
+    elements.append(format_url(item))
+    return elements
 
 
 def format_book_section_citation(item: dict) -> str:
     """Harvard reference style for book sections:
-    
+
     Surname, Initial(s). (Year) Title of chapter, in Surname of the editor(s), Initial(s) (ed.) Book title in italics. Edition. Place: Publisher, Page.
     """
-    year = format_date(item.get("date"))
-    editors = format_creators(item.get("creators"), creator_type="editor")
-    book_title = item.get("bookTitle")
-    place = item.get("place")
-    publisher = item.get("publisher")
-    pages = item.get("pages")
-
-    citation_elements = [
-        format_creators(item.get("creators")), 
-        f"({year})" if year else "", 
-        f'"{item.get("title")}"', 
-        f'in {editors} (ed.)' if editors else "",
-        f'*{book_title}*.' if book_title else "",
-        item.get("edition"),
-        place + ":" if place else "",
-        publisher + "," if publisher else "",
-        pages + "." if pages else "",
-        format_url(item)
+    elements = [
+        format_creators(item.get("creators")),
+        f"({format_date(item.get('date'))})",
+        f'"{item.get("title")}"',
     ]
 
-    formatted_citation = " ".join(elem for elem in citation_elements if elem != "")
-    return formatted_citation
+    if editors := format_creators(item.get("creators"), creator_type="editor"):
+        elements.append(f"in {editors} (ed.)")
+
+    if book_title := item.get("bookTitle"):
+        elements.append(f"*{book_title}*.")
+    if edition := item.get("edition"):
+        elements.append(edition)
+
+    if (place := item.get("place")) and (publisher := item.get("publisher")):
+        elements.append(f"{place}: {publisher},")
+
+    if pages := item.get("pages"):
+        elements.append(pages + ".")
+
+    elements.append(format_url(item))
+
+    return elements
 
 
 def format_book_citation(item: dict) -> str:
     """Harvard reference style for books:
-    
+
     Author surname, initial. (Year) *Book title*. Edition. Place: Publisher.
     """
-    citation_elements = []
+    elements = []
 
-    citation_elements.append(format_creators(item.get("creators")))
-    citation_elements.append(f'({format_date(item.get("date"))})')
-    citation_elements.append(f"*{item.get('title')}*.")
+    elements.append(format_creators(item.get("creators")))
+    elements.append(f"({format_date(item.get('date'))})")
+    elements.append(f"*{item.get('title')}*.")
 
-    if edition := item.get("edition"): 
-        citation_elements.append(edition + ".")
-    
-    if city := item.get("place"): 
-        citation_elements.append(city + ":")
-    if publisher := item.get("publisher"): 
-        citation_elements.append(publisher + ".")
+    if edition := item.get("edition"):
+        elements.append(edition + ".")
 
-    citation_elements.append(format_url(item))
+    if city := item.get("place"):
+        elements.append(city + ":")
 
-    return " ".join(elem for elem in citation_elements if elem != "")
+    if publisher := item.get("publisher"):
+        elements.append(publisher + ".")
+
+    elements.append(format_url(item))
+
+    return elements
 
 
 def format_article_citation(item: dict) -> str:
@@ -100,24 +103,30 @@ def format_article_citation(item: dict) -> str:
     Surname, Initial(s). (Year of Publication) Title of paper, Title of the conference in italics. Place and date of the conference. Place published: Publisher, Page.
     """
     elements = []
-    
+
     elements.append(format_creators(item.get("creators")))
-    elements.append(f'({format_date(item.get("date"))})')
+    elements.append(f"({format_date(item.get('date'))})")
     elements.append(f'"{item.get("title")}",')
+
     if proceedings := item.get("proceedingsTitle"):
         elements.append(f"*{proceedings}*,")
+
     if journal := item.get("publicationTitle"):
         elements.append(f"*{journal}*,")
+
     if (volume := item.get("volume")) and (issue := item.get("issue")):
         elements.append(f"{volume}({issue}),")
+
     if pages := item.get("pages"):
         elements.append(f"{pages}.")
+
     if doi := item.get("doi"):
         elements.append(f"doi:{doi}.")
-    
+
     elements.append(format_url(item))
 
-    return " ".join(elements)
+    return elements
+
 
 def format_creators(authors: list, creator_type="author") -> str:
     """Format author names in Harvard reference style."""
@@ -133,21 +142,21 @@ def format_creators(authors: list, creator_type="author") -> str:
         case 1:
             authorlist = _authors[0]
         case 2:
-            authorlist =  " and ".join(_authors)
-        case 3: 
-             authorlist = ", ".join(_authors[:2]) + ", og " + _authors[2]
+            authorlist = " and ".join(_authors)
+        case 3:
+            authorlist = ", ".join(_authors[:2]) + ", og " + _authors[2]
         case _:
-             authorlist = _authors[0] + " et al."
+            authorlist = _authors[0] + " et al."
     return authorlist
 
 
 def format_date(date: str, full: bool = False) -> str | None:
     """Format date in Harvard reference style."""
     try:
-        if full: 
-            date  = datetime.fromisoformat(date).strftime("%Y-%m-%d")
-        else: 
-            date  = datetime.fromisoformat(date).year
+        if full:
+            date = datetime.fromisoformat(date).strftime("%Y-%m-%d")
+        else:
+            date = datetime.fromisoformat(date).year
     except ValueError:
         date = date.split("-")[0]
     return date if date else ""
@@ -159,13 +168,13 @@ def format_url(data: dict) -> str | None:
     if not url:
         return ""
 
-    formatted_url = f"Tilgjengelig fra: {url}"
+    formatted_url = f"\nTilgjengelig fra: {url}"
 
-    if access_date == "": 
+    if access_date == "":
         return formatted_url
-    
+
     date = format_date(access_date, full=True)
-    formatted_url += f" (Hentet: {date})" 
+    formatted_url += f" (Hentet: {date})"
     return formatted_url
 
 
@@ -182,35 +191,46 @@ def generate_citations(library_id, library_type, api_key, output_path):
     """
 
     zot = zotero.Zotero(library_id, library_type, api_key)
-    items = zot.collection_items("JWSHLR3R")
+    items = [
+        item
+        for item in zot.collection_items("JWSHLR3R")
+        if item["meta"].get("parsedDate") is not None
+    ]
 
     # Reverse sort by date
     # items = sorted(items, key=lambda d: (d["meta"]["parsedDate"].split('-')[0], d['data']['creators'][0]['lastName'] ), reverse=True)
+    items = sorted(
+        items,
+        key=lambda d: (
+            d["meta"]["parsedDate"].split("-")[0],
+            d["data"]["creators"][0]["lastName"],
+        ),
+        reverse=True,
+    )
     formatted_citations = []
-    # Format in reference style 
+    # Format in reference style Harvard
     for item in items:
         data = item["data"]
+        print(data)
         item_type = data["itemType"]
-        formatted_citation = ""
+
         match item_type:
             case "journalArticle" | "conferencePaper":
-                formatted_citation = format_article_citation(data)
+                citation_elements = format_article_citation(data)
             case "book":
-                formatted_citation = format_book_citation(data)
+                citation_elements = format_book_citation(data)
             case "thesis":
-                formatted_citation = format_thesis_citation(data)
+                citation_elements = format_thesis_citation(data)
             case "bookSection":
-                formatted_citation = format_book_section_citation(data)
+                citation_elements = format_book_section_citation(data)
             case "newspaperArticle":
-                formatted_citation = format_newspaper_article_citation(data)
+                citation_elements = format_newspaper_article_citation(data)
             case _:
                 # Notes or attachments are not interesting for citation
                 print(f"Item type {item_type} not supported.")
                 continue
 
-        #citation_elements = [authors, date, title, pub, url, doi]
-        #citation = " ".join(elem for elem in citation_elements if elem != "")
-
+        formatted_citation = " ".join(citation_elements)
         formatted_citations.append(formatted_citation)
 
     with open(output_path, "w+") as f:
